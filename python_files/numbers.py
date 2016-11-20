fd5b3a9a33e5f8c6932b7431f4a257283d51e372
kleczekr <kleczek.r@gmail.com>
2016-11-20 15:33:00 +0100

    Update numbers.py
    Comment

--- python_files/numbers.py
+++ python_files/numbers.py
@@ -9,6 +9,8 @@ with open('ways_tags.csv') as f:
     for row in reader:
         if row['key'] == 'street' and numbers(row['value']):
             new_set.add(row['value'])
+    # The set cannot simply be printed, as the Unicode characters would not be displayed properly.
+    # The following loop is created specifically to display the names nicely, with Unicode characters and all.
     for value in new_set:
         print unicode(value, 'utf-8').encode('utf-8')
 
